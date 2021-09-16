f90ee5c6cca7d3e96fe6a864e6b39aac48fd2c82
sohanrai09 <89385413+sohanrai09@users.noreply.github.com>
2021-09-16 16:05:04 +0530

    Update norns_BGP.py
    added script exec time

--- norns_BGP.py
+++ norns_BGP.py
@@ -27,7 +27,7 @@ def BGP_peer_fn(BGP_x):
             else:
                 print(f"Peer_{BGP_x[item]['peers'][peer]['remote_as']} {peer} ({item}) is DOWN!\n")
 
-router_list = dict.keys(output) # Nornir output is a Dict with 'hosts' in hosts.yaml as keys
+router_list = dict.keys(output) # Nornir output is a Dict with 'hosts(keys)' in hosts.yaml as keys
 
 for router in router_list:
     print(f"~~~~~~~ BGP Status on  {router} ~~~~~~~\n")
